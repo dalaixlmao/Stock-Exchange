f9162a15dcc75cd28749b0a8005823ac876b25cb
Anubhav <anubhav_a@me.iitr.ac.in>
2025-01-24 21:18:57 +0530

    Exported type of schemas

---
+++ packages/type/src/index.ts
@@ -0,0 +1,26 @@
+import zod from "zod";
+
+class ZodSchema {
+  userSignupSchema = zod.object({
+    name: zod.string(),
+    email: zod.string().email(),
+    password: zod.string().min(8),
+  });
+
+  userLoginSchema = this.userSignupSchema.omit({ name: true });
+
+  orderSchema = zod.object({});
+
+  validate<Type>(schema: zod.ZodType<Type>, object: Type) {
+    const result = schema.safeParse(object);
+    return result.success;
+  }
+}
+
+const temp: ZodSchema = new ZodSchema();
+type userSignupType = zod.infer<typeof temp.userSignupSchema>;
+type userLoginType = zod.infer<typeof temp.userLoginSchema>;
+type orderType = zod.infer<typeof temp.orderSchema>;
+
+export default ZodSchema;
+export type { userLoginType, userSignupType, orderType };
